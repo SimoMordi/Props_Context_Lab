567f593ff9ab44368320e976981c31968e9b2428
simo <simo.mordi@gmail.com>
2023-09-18 16:24:59 -0700

    new

--- props_app/src/App.jsx
+++ props_app/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Route, Routes } from "react-router-dom";
 import "./styles.css";
 import Products from "./pages/Products";
@@ -6,8 +5,13 @@ import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 
 function App() {
-
-
+  const [products, setProducts] = useState([
+    { name: "Laptop", price: 1000, inCart: false },
+    { name: "Smartphone", price: 500, inCart: false },
+    { name: "Headphones", price: 100, inCart: false },
+    { name: "Keyboard", price: 50, inCart: false },
+    { name: "Mouse", price: 30, inCart: false },
+  ]);
 
   return (
     <div>
