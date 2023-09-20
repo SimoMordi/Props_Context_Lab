cc13577bd1f2a887db5f8544e9b888449314c467
simo <simo.mordi@gmail.com>
2023-09-19 17:23:02 -0700

    new

--- props_app/src/components/ProductsDisplay/index.jsx
+++ props_app/src/components/ProductsDisplay/index.jsx
@@ -1,22 +1,29 @@
 
-const ProductsDisplay = () => {
+const ProductsDisplay = ({products, setProducts}) => {
 
     const handleProductClick = (id) => {
-        // we have the id of the clicked property
-        
+      setProducts(products.map(product => 
+          product.id === id ? {...product, inCart: true} : product
+      ));
+    };
+    
+    return (
+        <div>
+            {products.map((product) => {
+                return <div key={product.name} onClick={() => handleProductClick(product.name)}>{product.name}</div>
+            })}
+        </div>
+    )
+}
+
+export default ProductsDisplay;
+
+
+
+      
         // 1. make a new products array (from the old state)
         // 2. set inCart = true for the clicked product
         // 3. set state to this new products array
-    }
+    
 
-  return (
-  <div>
-        {/* we need the products array here */}
-        {products.map((product) => {
-            return <div onClick={() => handleProductClick(product.id)}>product.name</div>
-        })}
-  </div>
-  )
-}
-
-export default ProductsDisplay
+  
